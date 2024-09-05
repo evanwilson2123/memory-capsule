8350a473108899e5fc75d28a1424c852ce1461b1
evanwilson2123 <evanmwilson@gmail.com>
2024-09-04 20:37:08 -0400

    hide locked capsules

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -106,6 +106,13 @@ const Profile = () => {
     router.push(`/view-capsule/${id}`);
   };
 
+  // Helper to check if the unlock date has passed
+  const isUnlocked = (unlockDate: Date) => {
+    const currentDate = new Date();
+    // const capsuleUnlockDate = new Date(unlockDate);
+    return currentDate >= new Date(unlockDate);
+  };
+
   return (
     <div className="profile-container min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex flex-col items-center justify-center p-6">
       <h1 className="text-4xl font-extrabold text-center text-white mb-8">
@@ -195,77 +202,93 @@ const Profile = () => {
       {/* Time Capsules Received */}
       <h2 className="text-2xl font-semibold text-white mb-6">Received</h2>
       {profile && profile.timeCapsulesReceived.length > 0 ? (
-        profile.timeCapsulesReceived.map((capsule) => (
-          <div
-            key={capsule._id as string}
-            onClick={() => handleCapsuleClick(capsule._id as string)}
-            className="bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl mb-8 cursor-pointer transform hover:scale-105 transition-transform duration-300"
-          >
-            <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
-              {capsule.title}
-            </h1>
-            <p className="text-gray-700 text-center mb-8 text-lg">
-              {capsule.description}
-            </p>
-            <div className="mb-8">
-              <h2 className="text-2xl text-center font-semibold text-gray-700 mb-4">
-                Message
-              </h2>
-              <p className="text-gray-800 p-4 bg-gray-100 rounded-lg shadow-inner text-justify leading-relaxed">
-                {capsule.message}
+        profile.timeCapsulesReceived.map((capsule) => {
+          const unlocked = isUnlocked(capsule.unlockDate);
+          return (
+            <div
+              key={capsule._id as string}
+              onClick={() => handleCapsuleClick(capsule._id as string)}
+              className={`bg-white p-8 rounded-lg shadow-lg w-full max-w-2xl mb-8 cursor-pointer transform hover:scale-105 transition-transform duration-300 ${
+                unlocked ? "cursor-pointer" : "cursor-not-allowed"
+              }`}
+            >
+              <h1 className="text-3xl font-bold text-center text-blue-600 mb-6">
+                {capsule.title}
+              </h1>
+              <p className="text-gray-700 text-center mb-8 text-lg">
+                {capsule.description}
               </p>
+
+              {/* Locked State */}
+              {!unlocked ? (
+                <div className="text-center text-red-500 text-xl">
+                  This time capsule from {capsule.senderName} is locked until
+                  {new Date(capsule.unlockDate).toLocaleDateString()}
+                </div>
+              ) : (
+                <div>
+                  <div className="mb-8">
+                    <h2 className="text-2xl text-center font-semibold text-gray-700 mb-4">
+                      Message
+                    </h2>
+                    <p className="text-gray-800 p-4 bg-gray-100 rounded-lg shadow-inner text-justify leading-relaxed">
+                      {capsule.message}
+                    </p>
+                  </div>
+                  <div className="media-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {capsule.images.length > 0 && (
+                      <div>
+                        <h3 className="text-lg font-bold text-gray-700 mb-2">
+                          Images
+                        </h3>
+                        {capsule.images.map((url: string, index: number) => (
+                          <img
+                            key={index}
+                            src={url}
+                            alt="Memory Image"
+                            className="w-full h-auto rounded-lg shadow-lg object-cover"
+                          />
+                        ))}
+                      </div>
+                    )}
+                    {capsule.videos.length > 0 && (
+                      <div>
+                        <h3 className="text-lg font-bold text-gray-700 mb-2">
+                          Videos
+                        </h3>
+                        {capsule.videos.map((url: string, index: number) => (
+                          <video
+                            key={index}
+                            controls
+                            className="w-full h-auto rounded-lg shadow-lg"
+                          >
+                            <source src={url} type="video/mp4" />
+                          </video>
+                        ))}
+                      </div>
+                    )}
+                    {capsule.audios.length > 0 && (
+                      <div>
+                        <h3 className="text-lg font-bold text-gray-700 mb-2">
+                          Audios
+                        </h3>
+                        {capsule.audios.map((url: string, index: number) => (
+                          <audio
+                            key={index}
+                            controls
+                            className="w-full rounded-lg shadow-lg"
+                          >
+                            <source src={url} type="audio/mpeg" />
+                          </audio>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
-            <div className="media-content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {capsule.images.length > 0 && (
-                <div>
-                  <h3 className="text-lg font-bold text-gray-700 mb-2">
-                    Images
-                  </h3>
-                  {capsule.images.map((url: string, index: number) => (
-                    <img
-                      key={index}
-                      src={url}
-                      alt="Memory Image"
-                      className="w-full h-auto rounded-lg shadow-lg object-cover"
-                    />
-                  ))}
-                </div>
-              )}
-              {capsule.videos.length > 0 && (
-                <div>
-                  <h3 className="text-lg font-bold text-gray-700 mb-2">
-                    Videos
-                  </h3>
-                  {capsule.videos.map((url: string, index: number) => (
-                    <video
-                      key={index}
-                      controls
-                      className="w-full h-auto rounded-lg shadow-lg"
-                    >
-                      <source src={url} type="video/mp4" />
-                    </video>
-                  ))}
-                </div>
-              )}
-              {capsule.audios.length > 0 && (
-                <div>
-                  <h3 className="text-lg font-bold text-gray-700 mb-2">
-                    Audios
-                  </h3>
-                  {capsule.audios.map((url: string, index: number) => (
-                    <audio
-                      key={index}
-                      controls
-                      className="w-full rounded-lg shadow-lg"
-                    >
-                      <source src={url} type="audio/mpeg" />
-                    </audio>
-                  ))}
-                </div>
-              )}
-            </div>
-          </div>
-        ))
+          );
+        })
       ) : (
         <p className="text-white text-center text-lg">
           No time capsules received.
